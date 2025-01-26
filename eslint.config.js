f525014d00aa25e886d146fcc65ad26fbac91b97
Mario C <mariocs@gmail.com>
2025-01-26 19:22:28 +0100

    Improve ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -9,9 +9,29 @@ module.exports = [
     },
     plugins: {
       "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      "import": require("eslint-plugin-import"),
     },
     rules: {
       indent: ["error", 2],
+      "no-unused-vars": "off", // Turn off base rule to avoid conflicts
+      "@typescript-eslint/no-unused-vars": ["error", { 
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_" // Also ignore destructured variables starting with _
+      }],
+      "sort-imports": ["error", {
+        ignoreCase: true,
+        ignoreDeclarationSort: true, // Because we're using import/order
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+      }],
+      "import/order": ["error", {
+        "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
+        "newlines-between": "always",
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }]
     },
   },
 ]; 
